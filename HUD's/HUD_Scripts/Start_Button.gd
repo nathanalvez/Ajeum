e67e67ed2883cce7e9a924f1d7600ee552e2d26a
nathanalvez <nathan_fa@live.com>
2022-12-08 20:15:02 -0300

    Merge

--- HUD's/HUD_Scripts/Start_Button.gd
+++ HUD's/HUD_Scripts/Start_Button.gd
@@ -1,13 +1,12 @@
 extends Button
 
-
-func _on_Start_pressed():
-	pass # Replace with function body.
-	get_tree().change_scene("res://Scenes/Main.tscn")
-
 func _on_Crditos_pressed():
 	pass # Replace with function body.
 
 func _on_Sair_pressed():
 	pass # Replace with function body.
 	get_tree().quit()
+
+
+func _on_Start_Button_pressed():
+	get_tree().change_scene("res://Scenes/Main.tscn")
